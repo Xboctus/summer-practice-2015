3369f2d900daaaab89655fbc5743a8ab4c2afb68
sergej <sergej.gorodnichev@gmail.com>
2015-07-03 14:35:54 +0300

    1st homework

---
+++ Gorodnichev-Sergej/HW1/src/ActiveEvent.java
@@ -0,0 +1,33 @@
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ * Created by sergej on 30.06.15.
+ */
+public class ActiveEvent implements Comparable<ActiveEvent> {
+    private String username;
+    private Date date;
+    private String text;
+
+    public ActiveEvent(String username, Date date, String text) {
+        this.username = username;
+        this.date = date;
+        this.text = text;
+    }
+
+    public int compareTo(ActiveEvent obj) {
+        return date.compareTo(obj.date);
+    }
+
+    public long getTime() {
+        return date.getTime();
+    }
+
+    public void print() {
+        Date now = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("dd.MM.yyyy'-'HH:mm:ss");
+        System.out.println(ft.format(now) + " " + username + " " + text + ";");
+    }
+}
+
