680602f223163a6c30f072242410bf635ebfddee
sergej <sergej.gorodnichev@gmail.com>
2015-07-24 01:40:28 +0300

    fix

--- Gorodnichev-Sergej/Storage/src/main/java/com/threeSergei/AdminController.java
+++
@@ -1,7 +0,0 @@
-package com.threeSergei;
-
-/**
- * Created by sergej on 15.07.15.
- */
-public class AdminController {
-}
--- Gorodnichev-Sergej/Storage/src/main/java/com/threeSergei/model/Store.java
+++
@@ -1,7 +0,0 @@
-package com.threeSergei.model;
-
-/**
- * Created by sergej on 15.07.15.
- */
-public class Store {
-}
